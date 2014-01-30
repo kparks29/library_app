fafd6d6352a93d5daf64017006508cc61b861556
Kendrick Parks <bordersmbk02@aol.com>
2014-01-30 10:05:39 -0800

    added scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 LibraryApp::Application.routes.draw do
+  resources :readers
+
+  resources :librarians
+
+  resources :books
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
